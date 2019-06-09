8dd4e85d8dffc4515cda304f885c90728744d91f
Rafał <heller.rafal@gmail.com>
2019-06-10 01:22:59 +0200

    create Main section on desktop

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,8 +5,15 @@ class Main extends Component {
         return (
             <>
                 <div id='thanks' className='thanks-section'>
+
+                    <p className='thanks-text'>Dziękujemy za przesłanie formularza
+                        Na maila prześlemy wszelkie
+                        informacje o odbiorze.
+                        <div className="decoration"></div>
+                    </p>
+
                 </div>
-                <div className="name">nemememe</div>
+
             </>
         );
     }
