8ac40c44b01fa28b2842c0762deaaa534f6f0055
Rafał <heller.rafal@gmail.com>
2019-06-11 01:50:49 +0200

    add section with steps

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,10 @@ import React, {Component} from 'react';
 import Menu from './Menu'
 import Title from './Title'
 import Rectangles from './Rectangles'
+import hands from './../images/28.png'
+import arrow from './../images/016.png'
+import eyes from './../images/039.png'
+import courier from './../images/58.png'
 
 // import Minimenu from "./Minimenu";
 
@@ -63,6 +67,33 @@ class Header extends Component {
                             quos, suscipit.</p>
                     </div>
                 </section>
+                <main id='register'>
+                    <p>Wystarczą 4 proste kroki</p>
+                    <div className="decoration"></div>
+                    <div className="steps-container">
+                        <div className="step">
+                            <img src={hands} alt="hands"/>
+                            <h3>Wybierz rzeczy</h3>
+                            <p>ubrania, zabawki,
+                                sprzęt i inne</p>
+
+                        </div>
+                        <div className="step"><img src={arrow} alt="hands"/>
+                            <h3>Wybierz rzeczy</h3>
+                            <p>ubrania, zabawki,
+                                sprzęt i inne</p></div>
+                        <div className="step"><img src={eyes} alt="hands"/>
+                            <h3>Wybierz rzeczy</h3>
+                            <p>ubrania, zabawki,
+                                sprzęt i inne</p></div>
+                        <div className="step"><img src={courier} alt="hands"/>
+                            <h3>Wybierz rzeczy</h3>
+                            <p>ubrania, zabawki,
+                                sprzęt i inne</p></div>
+                    </div>
+
+                    <button>załóż konto</button>
+                </main>
 
             </>
         );
